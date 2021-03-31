4f1ab52267075b54a13c1ec0c203611bb8918298
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-03-31 11:55:15 +0000

    team added

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -7,14 +7,107 @@ declare var M: any;
 })
 export class TeamComponent implements OnInit, AfterViewInit {
 
+  team:any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createTeam();
   }
 
   ngAfterViewInit(){
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
+  }
+
+  createTeam(){
+    this.team = [
+      {
+        'name' : 'Rishabh Ranjan',
+        'img' : 'rishabh.jpg',
+        'position' : 'Chairman'
+      },
+      {
+        'name' : 'Shubham Gupta',
+        'img' : 'default.png',
+        'position' : 'Convenor'
+      },
+      {
+        'name' : 'Abhinav',
+        'img' : 'Abhinav.jpg',
+        'position' : 'Convenor'
+      },
+      {
+        'name' : 'Abhishek',
+        'img' : 'abhis.jpeg',
+        'position' : 'Coding Club Head'
+      },
+      {
+        'name' : 'Abhyam',
+        'img' : 'abhyam.jpg',
+        'position' : 'Robotics Club Head'
+      },
+      {
+        'name' : 'Akshat Mehra',
+        'img' : 'default.png',
+        'position' : 'Astronomy Club Head'
+      },
+      {
+        'name' : 'Amol Jaggi',
+        'img' : 'amol.jpg',
+        'position' : 'SAE Club Head'
+      },
+      {
+        'name' : 'Apurv',
+        'img' : 'Apurv.jpg',
+        'position' : 'Media Team Head'
+      },
+      {
+        'name' : 'Lovish Bains',
+        'img' : 'lovish.jpg',
+        'position' : 'Design Team Head'
+      },
+      {
+        'name' : 'Shreyas',
+        'img' : 'Shreyas.jpg',
+        'position' : 'Overall Event Head'
+      },
+      {
+        'name' : 'Dhiraj',
+        'img' : 'default.png',
+        'position' : 'Marketing & Sponsorship Head'
+      },
+      {
+        'name' : 'Abhishek Suman',
+        'img' : 'abhishek_suman.jpg',
+        'position' : 'Marketing & Sponsors'
+      },
+      {
+        'name' : 'Jaswant',
+        'img' : 'jaswant.jpg',
+        'position' : 'Networking & Outreach Head'
+      },
+      {
+        'name' : 'Priyanshu',
+        'img' : 'priyanshu.jpg',
+        'position' : 'Networking & Outreach Head'
+      },
+      {
+        'name' : 'Sachin',
+        'img' : 'sachin.jpg',
+        'position' : 'Event Head (Blockchain)'
+      },
+      {
+        'name' : 'Sai Vishnu',
+        'img' : 'sai_vishnu.jpg',
+        'position' : 'Event Head (Beacon)'
+      },
+      {
+        'name' : 'Siddharth',
+        'img' : 'default.png',
+        'position' : 'Event Head (Treasure Hunt)'
+      },
+    ]
   }
 
 }
